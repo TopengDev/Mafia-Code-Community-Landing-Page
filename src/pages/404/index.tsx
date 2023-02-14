d48ffe6db18ebb18eb8840c8da73128808e47b3c
TopengDev <topengdev@outlook.com>
2023-02-14 12:56:13 +0700

    added: error-404 page

---
+++ src/pages/404/index.tsx
@@ -0,0 +1,15 @@
+import { type NextPage } from "next";
+import { Error404Page } from "@/components/404Page";
+import Layout from "@/components/Layout";
+
+const ErrorPage: NextPage = () => {
+  return (
+    <>
+      <Layout title="error-404 | Page not found">
+        <Error404Page />
+      </Layout>
+    </>
+  );
+};
+
+export default ErrorPage;
